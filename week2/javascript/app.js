c77d84eca782aa848eb6db54fdfde84a70ff3fad
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-01-14 10:48:26 +0800

    add: week2

---
+++ week2/javascript/app.js
@@ -0,0 +1,26 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login() {
+      const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      axios.post(api, this.user).then((response) => {
+        const { token, expired } = response.data;
+        // 寫入 cookie token
+        // expires 設置有效時間
+        document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
+        window.location = 'products.html';
+      }).catch((error) => {
+        alert(error.data.message);
+      });
+    },
+  },
+}).mount('#app');
---
+++ week2/javascript/products.js
@@ -0,0 +1,46 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'hexschoolvue',
+      products: [],
+      tempProduct: {},
+    }
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          alert(err.data.message)
+          window.location = 'index.html';
+        })
+    },
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+        .then((response) => {
+          this.products = response.data.products;
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        })
+    },
+    openProduct(item) {
+      this.tempProduct = item;
+    }
+  },
+  created() {
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin()
+  }
+}).mount('#app');
+
