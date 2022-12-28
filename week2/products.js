1c1246e20d173ecb45b47db033713a12fc7fcdb9
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-12-28 10:07:57 +0800

    feat: 🎸 上傳 week2

---
+++ week2/products.js
@@ -0,0 +1,45 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'hexschoolvue',
+      products: [],
+      tempProduct: {},
+    }
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getData();
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+          window.location = 'login.html';
+        })
+    },
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+        .then((response) => {
+          this.products = response.data.products;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    openProduct(item) {
+      this.tempProduct = item;
+    }
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin()
+  }
+}).mount('#app');
