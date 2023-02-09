9536e3470d07fa5bea092387d7d4e31086e41fb6
yinmin <alice0050722@gmail.com>
2023-02-09 09:37:32 +0800

    fix: 補 dist

---
+++ week6/dist/assets/Orders.13b28c92.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t}from"./index.a7ebf2d2.js";const n={name:"Orders",data(){return{}}};function o(s,a,c,_,d,p){return t(),r("div",null," \u9019\u88E1\u662F \u8A02\u55AE\u5217\u8868 ")}const l=e(n,[["render",o]]);export{l as default};
